b101b1db17001bda55273120f7292c174b979402
Suhas-098 <suhasmagadum05@gmail.com>
2025-12-04 08:41:08 +0530

    created new controller for restaurant partner controller

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -1,10 +1,13 @@
 import express from "express";
-import { registerUser, loginUser, logoutUser } from "../controllers/auth.controller.js";
+import { registerUser, loginUser, logoutUser, registerRestaurantPartner, loginRestaurantPartner, logoutRestaurantPartner } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.post("/user/register", registerUser);
 router.post("/user/login", loginUser);
 router.post("/user/logout", logoutUser);
+router.post("/restaurantPartner/register", registerRestaurantPartner);
+router.post("/restaurantPartner/login", loginRestaurantPartner);
+router.post("/restaurantPartner/logout", logoutRestaurantPartner);
 
 export default router;
