25fd7f19e111a7db2a5555a4ed3b3e6a8266d176
Suhas-098 <suhasmagadum05@gmail.com>
2025-12-06 12:40:22 +0530

    created a ui just ui

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Reels from '../components/Reels/Reels'
+import { useNavigate } from 'react-router-dom'
+
 
 function Home() {
+    const navigate = useNavigate();
+    useEffect(() => {
+        const hasToken = document.cookie.split(";").some(row => row.startsWith("token="));
+        if (!hasToken) {
+            navigate("/user/login");
+        }
+    }, []);
     return (
         <div>
             <div style={{ width: '100%', height: '100vh', overflow: 'hidden' }}>
                 <Reels />
+                <div style={{ position: 'absolute', top: '50%', left: '18%', transform: 'translate(-50%, -50%)' }}>
+                    <h1>Home Page</h1>
+                </div>
             </div>
-            <h1>Home</h1>
         </div>
     )
 }
